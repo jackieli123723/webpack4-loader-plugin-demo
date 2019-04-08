8f88299560a1cb3e5119237bb79b1c444019dd9d
jiangziguo <1320015977@qq.com>
2019-04-08 16:57:36 +0800

    hooks参数对比执行顺序

--- webpack-plugins/MyFisrtWbapckPlugin.js
+++ webpack-plugins/MyFisrtWbapckPlugin.js
@@ -1,17 +1,83 @@
 class MyFisrtWbapckPlugin {
    
    apply(compiler){
-      compiler.hooks.done.tapAsync("MyFisrtWbapckPlugin", (stats, cb) => {
-	     const res = [];
-		 for(let assetName in stats.compilation.assets) {
+   	//way1 ok
+   //    compiler.hooks.done.tapAsync("MyFisrtWbapckPlugin", (stats, cb) => {
+	  //    const res = [];
+		 // for(let assetName in stats.compilation.assets) {
+		 //     res.push(assetName);
+		 // }
+		 // console.log(res.join("\n"));
+		 // // 处理完毕后执行 callback 以通知 Webpack 
+   //       // 如果不执行 callback，运行流程将会一直卡在这不往下执行
+		 // cb();
+	  // })
+
+	    //way2 和上面结果一样不？ compilation.assets 空的 stats compilation Version: webpack 4.27.1
+	    //way2 和上面结果一样不？ compilation.compilation.assets 有值 stats compilation Version: webpack 4.12.0
+	    //way2 和上面结果一样不？ compilation.compilation.assets 有值 stats compilation Version: webpack 4.29.6
+	  //  compiler.hooks.done.tapAsync("MyFisrtWbapckPlugin", (compilation, cb) => {
+	  //    const res = [];
+		 // for(let assetName in compilation.compilation.assets) {
+		 //     res.push(assetName);
+		 // }
+		 // console.log(res.join("\n"));
+		 
+		 // cb();
+	  // })
+       
+
+       //结论  compiler.hooks.emit.tapAsync 生成资源到 output 目录之前钩子 参数：compilation 就可以用compilation.assets 访问到资源
+       //用 compiler.hooks.done.tapAsync  编译(compilation)完成钩子 参数：stats 用compilation.assets 访问不到资源 这是个异步必须 compilation.compilation.assets 这样用才能访问到资源
+       //参照上面的way1 
+       compiler.hooks.done.tapAsync("MyFisrtWbapckPlugin", (compilation, cb) => {
+	     const res = [11];
+		 for(let assetName in compilation.compilation.assets) {
 		     res.push(assetName);
 		 }
 		 console.log(res.join("\n"));
-		 // 处理完毕后执行 callback 以通知 Webpack 
-         // 如果不执行 callback，运行流程将会一直卡在这不往下执行
+		 
 		 cb();
 	  })
+
+       compiler.hooks.emit.tapAsync("MyFisrtWbapckPlugin", (compilation, cb) => {
+	     const res = [22];
+		 for(let assetName in compilation.assets) {
+		     res.push(assetName);
+		 }
+		 console.log(res.join("\n"));
+		 
+		 cb();
+	  })
+
+	  compiler.hooks.emit.tapAsync('MyFisrtWbapckPlugin', (compilation, callback) => {
+      // Create a header string for the generated file:
+      var filelist = 'In this build:\n\n';
+
+      // Loop through all compiled assets,
+      // adding a new line item for each filename.
+      for (var filename in compilation.assets) {
+        filelist += '- ' + filename + '\n';
+      }
+
+      // Insert this list into the webpack build as a new file asset:
+      compilation.assets['filelist'+ (+new Date()) +'.md'] = {
+        source: function() {
+          return filelist;
+        },
+        size: function() {
+          return filelist.length;
+        }
+      };
+
+      callback();
+    });
+
+
    }
 }
 
 module.exports = MyFisrtWbapckPlugin;
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -79,13 +79,13 @@ module.exports = {
     //use
     new MyFisrtWbapckPlugin(),
     // 在初始化 EndWebpackPlugin 时传入了两个参数，分别是在成功时的回调函数和失败时的回调函数；
-    new EndWebpackPlugin((stats) => {
-      // Webpack 构建成功，并且文件输出了后会执行到这里，在这里可以做发布文件操作
-      console.log('构建成功，并且文件输出了后会执行到这里，在这里可以做发布文件操作',stats.compilation.assets)
-    }, (err) => {
-      // Webpack 构建失败，err 是导致错误的原因
-      console.error(err);        
-    })
+    // new EndWebpackPlugin((stats) => {
+    //   // Webpack 构建成功，并且文件输出了后会执行到这里，在这里可以做发布文件操作
+    //   console.log('构建成功，并且文件输出了后会执行到这里，在这里可以做发布文件操作',stats.compilation.assets)
+    // }, (err) => {
+    //   // Webpack 构建失败，err 是导致错误的原因
+    //   console.error(err);        
+    // })
   ]
 };
 
